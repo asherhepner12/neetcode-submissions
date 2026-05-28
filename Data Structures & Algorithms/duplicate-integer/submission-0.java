c9b34ae6c629e79979b11d3036f99660c0aa4dee
asherhepner12 <164114192+asherhepner12@users.noreply.github.com>
2026-05-28 17:52:22 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (map.get(nums[i])!= null)
+            {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
